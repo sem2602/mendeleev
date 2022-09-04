ca7b8a4c948efa22ebdab0317166b1bf44153d68
sem2602 <sem2602@gmail.com>
2022-09-04 22:30:52 +0300

    modified main files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('home')
+
+    <h1>Home page!</h1>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,12 @@
 </head>
 <body>
 
-    @yield('content')
+    <div class="container">
+
+        @yield('content')
+
+    </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
 </body>
